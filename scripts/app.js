67c04ce8c3fb39ffe8275fbd5ee0ecda47ab5b89
oriofir <97814056+oriofir@users.noreply.github.com>
2022-02-10 12:15:04 -0800

    buttons don't decrease once dead

--- scripts/app.js
+++ scripts/app.js
@@ -42,9 +42,9 @@ $('.start').on('click', () => {
 
     // timers for levels
     // setInterval(ageLevel, 2000);
-    setInterval(hungerLevel, 2000);
-    setInterval(sleepLevel, 3000);
-    setInterval(boredomLevel, 2500);
+    setInterval(hungerLevel, 500);
+    setInterval(sleepLevel, 500);
+    setInterval(boredomLevel, 500);
     // setInterval(ageDeath, 2000);
 });
 
@@ -57,18 +57,18 @@ const ageLevel= (num) => {
 };
 
 const ageDeath = () => {
-    if(hunger === level[9] || sleepiness === level[9] || boredom === level[9]){
-        clearInterval(ageLevel())
-        $('#age').text('DEATH!!')
-        $('#age').css('background-color', 'red')
-    } else if(age === 10 || age === 20 || age === 30){
+    if(age === 10 && age === 20 && age === 30){
         $('#age').text('I GROW!') 
         $('#age').css('background-color', 'red')
-    } else if(age === 25 || age === 35 || age === 45){
+    } else if(age === 25 && age === 35 && age === 45){
         $('#age').text('I AM A DEMON, YOU CANNOT KILL ME!')
         $('#age').css('background-color', 'red')
-    } else if(age === 40 || age === 50 || age === 55 || age === 60){
+    } else if(age === 40 && age === 50 && age === 55 && age === 60){
         $('#age').text('I AM IMMORTAL AND FEAST ON THE BLOOD OF THE INNOCENT!')
+        $('#age').css('background-color', 'red')
+    } else if(hunger === level[9] && sleepiness === level[9] && boredom === level[9]){
+        clearInterval(ageLevel())
+        $('#age').text('DEATH!!')
         $('#age').css('background-color', 'red')
     }
 };
@@ -87,12 +87,12 @@ const ageDeath = () => {
  */
   
 const hungerLevel = () => {
-    
+    console.log(hunger)
     // create a loop to 10
     // for(let i = 0; i < level.length; i++){
         // an if statement that stops the loop
-        if(hunger !== level[9]){
-            hunger++;
+        if(hunger < level[9]){
+            hunger += 1;
     // accessing the DOM to show the looping numbers
     $('#hunger-level').text(`Hunger: ${hunger}`);
     // stopping the looping number with a boolean
@@ -108,8 +108,8 @@ const sleepLevel = () => {
     // create a loop to 10
     // for(let i = 0; i < level.length; i++){
         // an if statement that stops the array at 10
-        if(sleepiness !== level[9]){
-            sleepiness++;
+        if(sleepiness < level[9]){
+            sleepiness += 1;
          // accessing the DOM to show the looping numbers
         $('#sleepiness-level').text(`Sleepiness: ${sleepiness}`);
         } else if (sleepiness === level[9]){
@@ -125,8 +125,8 @@ const boredomLevel = () => {
     // create a loop to 10
     // for(let i = 0; i < level.length; i++){
         // an if statement that stops the array at 10
-        if(boredom !== level[9]){
-            boredom++;
+        if(boredom < level[9]){
+            boredom += 1;
     
     // accessing the DOM to show the looping numbers
     $('#boredom-level').text(`Boredom: ${boredom}`);
@@ -137,42 +137,64 @@ const boredomLevel = () => {
 };
 
 $('#feed').on('click', () =>{
-    console.log("It Worked!");
-    if(hunger > level[0]){
+    console.log(hunger, hunger <= level[7]);
+    if(hunger > level[0] && hunger <= level[7]){
         hunger--;
     } else if(hunger < level[0]){
         hunger === level[0]
-    } else if (hunger === level[9]){
-        document.getElementById('#feed').disabled = true;   
+   }
+    if (hunger < level[9]){
+   $('#hunger-level').text(`Hunger: ${hunger}`);
+    } else {
+        $('#hunger-level').text(`Death by hunger!!`);
     }
-    $('#hunger-level').text(`Hunger: ${hunger}`);
-
+   if (hunger === level[9]){
+   $('#feed').off('click', () => {
+       $('#feed').hide();
+    })
+ }
 });
+
 
 $('#soul-suck').on('click', () =>{
     console.log("It Worked!");
-    if(sleepiness > level[0]){
+    if(sleepiness > level[0] && sleepiness <= level[7]){
         sleepiness--;
     } else if(sleepiness < level[0]){
         sleepiness === level[0];
-    };    
-    $('#sleepiness-level').text(`Sleepiness: ${sleepiness}`);
+    } 
+    if (sleepiness < level[9]){
+        $('#sleepiness-level').text(`Sleepiness: ${sleepiness}`);
+    } else {
+        $('#sleepiness-level').text(`Death by fatigue!!`);
+    }
+    if (sleepiness === level[9]){
+        $('#soul-suck').off('click', () => {
+            $('#soul-suck').hide();
+         })
+      }
+     });
 
-});
+
 
 $('#murder').on('click', () =>{
     console.log("It Worked!");
-    if(boredom > level[0]){
+    if(boredom > level[0] && boredom <= level[7]){
         boredom--;
     } else if(boredom < level[0]){
         boredom === level[0];
     } 
-    
-    $('#boredom-level').text(`Boredom: ${boredom}`);
-    // make an event unable to click murder anymore
-
-});
-
+    if (boredom < level[9]){
+        $('#boredom-level').text(`Boredom: ${boredom}`);
+    } else {
+        $('#boredom-level').text(`Death by boredom!!`);
+    }
+    if (boredom === level[9]){
+        $('#muder').off('click', () => {
+            $('#murder').hide();
+         })
+      }
+     });
 
 
 const updateTimer = () => {
@@ -187,16 +209,16 @@ const updateTimer = () => {
         ageLevel();
         ageDeath();
         setInterval(ageDeath, 3000);
-        
-    
         // hunger++
         // sleepiness++
         // boredom++
     } ;
-// hungerLevel();
 
+/* cycling through images */
+const imageBaby = () => {
+    $('#baby-demon').hide()
 
-
+}
 // if age goes to 70, end game
 
 // if hunger gets to 10, end game
